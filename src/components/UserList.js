1974b4816db2a67106a2e2ee0b21ad0804879678
ajat91.sudrajat <ajat91.sudrajat@gmail.com>
2023-01-03 13:28:28 +0700

    create component

---
+++ src/components/UserList.js
@@ -0,0 +1,52 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+import * as React from "react";
+
+const UserList = () => {
+  const [users, setUser] = useState([]);
+
+  useEffect(() => {
+    getUsers();
+  }, []);
+
+  const getUsers = async () => {
+    const val = await axios.get(
+      "https://http-nodejs-production-6360.up.railway.app/users"
+    );
+    setUser(val.data);
+  };
+
+  return (
+    <div className="columns">
+      <div className="column is-half">
+        <table className="table is-striped is-fullwidth mt-5">
+          <thead>
+            <tr>
+              <th>No</th>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Gender</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {users.map((val, idx) => (
+              <tr key={val.id}>
+                <th>{idx+1}</th>
+                <th>{val.name}</th>
+                <th>{val.email}</th>
+                <th>{val.gender}</th>
+                <th>
+                    <button className="button is-info is-small mr-2">Edit</button>
+                    <button className="button is-danger is-small">Delete</button>
+                </th>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+};
+
+export default UserList;
