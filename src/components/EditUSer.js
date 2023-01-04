b0ad290b61234e576ee3e6c52b03445af6eb2bb5
ajat91.sudrajat <ajat91.sudrajat@gmail.com>
2023-01-04 12:19:17 +0700

    create delete form

--- src/components/EditUSer.js
+++ src/components/EditUSer.js
@@ -24,7 +24,7 @@ const EditUser = () => {
   useEffect (()=>{
     //const data ={name,email,gender}
     getById();
-  },[name,email,gender]);
+  },[name]);
 
   const updateDataUser=async (e)=>{
       e.preventDefault();
